eb0c7f9776ecda76e958409e6419e55e600131af
crisis-argha <namespace.arg@gmail.com>
2024-11-23 02:26:52 +0530

    CRUD Local Storage(Database code)

---
+++ lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,17 @@
+class DataBaseAllreadyOpenException implements Exception {}
+
+class UnableToGetDocumentDirectory implements Exception {}
+
+class DatabaseIsNotOpen implements Exception {}
+
+class CouldNotDeleteUser implements Exception {}
+
+class UserAllreadyExists implements Exception {}
+
+class CouldNotFindUser implements Exception {}
+
+class CouldNotDeleteNote implements Exception {}
+
+class CouldNotFindNote implements Exception {}
+
+class CouldNotUpdateNote implements Exception {}
